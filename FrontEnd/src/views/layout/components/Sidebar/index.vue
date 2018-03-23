11ba15db0db542528fe3c0cfd898a9f5f5cdab55
Yi <mail@imliuyi.com>
2018-03-23 08:33:00 +0800

    add frontend

---
+++ FrontEnd/src/views/layout/components/Sidebar/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <scroll-bar>
+    <el-row justify="center">
+      <div style="text-align: center;"><img :src="logo" style="display:inline-block;margin: 20% auto; max-width:70%"></div>
+    </el-row>
+    <el-menu mode="vertical" unique-opened :default-active="$route.path" :collapse="isCollapse" background-color="#f5f5f5" active-text-color="#409EFF">
+      <sidebar-item :routes="routes"></sidebar-item>
+    </el-menu>
+  </scroll-bar>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import SidebarItem from './SidebarItem'
+import ScrollBar from '@/components/ScrollBar'
+import logo from '@/assets/logo.png'
+
+export default {
+  data() {
+    return {logo};
+  },
+  components: { SidebarItem, ScrollBar },
+  computed: {
+    ...mapGetters([
+      'sidebar'
+    ]),
+    routes() {
+      return this.$router.options.routes
+    },
+    isCollapse() {
+      return !this.sidebar.opened
+    }
+  }
+}
+</script>
