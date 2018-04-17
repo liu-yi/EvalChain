36eb8b530f1100f17877e484b1925a65aa1f2d64
Yi <mail@imliuyi.com>
2018-04-17 16:58:09 +0800

    0417

---
+++ FrontEnd/src/views/ongoing/index.vue
@@ -0,0 +1,73 @@
+<template>
+    <el-row>
+        <el-col :span="8" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 2 : 0">
+            <el-card :body-style="{ padding: '0px' }">
+                <img class="image" :src="hamburger">
+                <div style="padding: 14px;">
+                    <span>好吃的汉堡</span>
+                    <div class="bottom clearfix">
+                        <time class="time">{{ currentDate }}</time>
+                        <el-button type="text" class="button">操作按钮</el-button>
+                    </div>
+                </div>
+            </el-card>
+        </el-col>
+    </el-row>
+</template>
+
+<script>
+import hamburger from '@/assets/hamburger.png'
+export default {
+  name: 'ongoing',
+  data() {
+    return {
+      hamburger
+    }
+  }
+
+}
+</script>
+
+
+
+<style>
+.time {
+  font-size: 13px;
+  color: #999;
+}
+
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
+
+.button {
+  padding: 0;
+  float: right;
+}
+
+.image {
+  width: 100%;
+  display: block;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      currentDate: new Date()
+    }
+  }
+}
+</script>
