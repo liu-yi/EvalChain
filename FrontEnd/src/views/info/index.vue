0cf2eea0110bc771393080cfddbbc11365267d63
Yi <mail@imliuyi.com>
2018-04-26 08:07:04 +0800

    update

---
+++ FrontEnd/src/views/info/index.vue
@@ -0,0 +1,154 @@
+<template>
+    <div class="app-container" style="padding-top: 60px;">
+        <el-form ref="infoForm" :model="infoForm" :rules="infoRules" label-width="180px" style="width:800px;">
+            <el-form-item label="Name" prop="name">
+                <el-input v-model="infoForm.name" disabled=""></el-input>
+            </el-form-item>
+            <el-form-item label="Student ID" prop="stuid">
+                <el-input v-model="infoForm.stuid" disabled=""></el-input>
+            </el-form-item>
+            <el-form-item label="E-mail" prop="email">
+                <el-input v-model="infoForm.email" :class="{toVerifyInput: infoForm.changeEmail}" :disabled="infoForm.changePassword||infoForm.changePublicKey"></el-input>
+                <el-button v-if="infoForm.changeEmail" :class="{toVerifyButton: infoForm.changeEmail}" :disabled="infoForm.changePassword||infoForm.changePublicKey">Send E-mail to Verify</el-button>
+            </el-form-item>
+            <el-row>
+                <el-col :span="8">
+                    <el-form-item label="Change Password" prop="changePassword">
+                        <el-switch v-model="infoForm.changePassword" :disabled="infoForm.changePublicKey"></el-switch>
+                    </el-form-item>
+                </el-col>
+                <el-col :span="16">
+                    <el-form-item label="Change Public Key" prop="changePublicKey">
+                        <el-switch v-model="infoForm.changePublicKey" :disabled="infoForm.changePassword"></el-switch>
+                    </el-form-item>
+                </el-col>
+            </el-row>
+            <el-form-item label="Current Password" v-if="infoForm.changePassword||infoForm.changePublicKey" prop="currentPassword">
+                <el-input type="password" v-model="infoForm.currentPassword" placeholder="" auto-complete="off" :required="infoForm.changePassword||infoForm.changePublicKey"></el-input>
+            </el-form-item>
+            <el-form-item label="New Password" v-if="infoForm.changePassword" prop="newPassword">
+                <el-input type="password" v-model="infoForm.newPassword" placeholder="" auto-complete="off" :required="infoForm.changePassword"></el-input>
+            </el-form-item>
+            <el-form-item label="Confirm New Password" v-if="infoForm.changePassword" prop="checkPassword">
+                <el-input type="password" v-model="infoForm.checkPassword" placeholder="" auto-complete="off" :required="infoForm.changePassword"></el-input>
+            </el-form-item>
+            <el-form-item label="New Public Key" v-if="infoForm.changePublicKey" prop="publicKey">
+                <el-input type="textarea" v-model="infoForm.publicKey" :rows="4"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="onSubmit">Submit</el-button>
+                <el-button @click="resetForm('infoForm')">Reset</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    var validateEmail = (rule, value, callback) => {};
+    var validateCheckPassword = (rule, value, callback) => {
+      if (this.infoForm.changePassword) {
+        if (value === "") {
+          callback(new Error("Please input the password again!"));
+        } else if (value !== this.infoForm.newPassword) {
+          callback(
+            new Error("The password is different from the new password!")
+          );
+        } else {
+          callback();
+        }
+      }
+    };
+    var validateCurrentPassword = (rule, value, callback) => {
+      if (this.infoForm.changePassword || this.infoForm.changePublicKey) {
+        if (value === "") {
+          callback(new Error("Please input the current password!"));
+        }
+      }
+    };
+    var validateNewPassword = (rule, value, callback) => {
+      if (this.infoForm.changePassword) {
+        if (value === "") {
+          callback(new Error("Please input the current password!"));
+        }
+      }
+    };
+    return {
+      infoForm: {
+        name: "刘逸",
+        stuid: "11410601",
+        email: "11410601@mail.sustc.edu.cn",
+        changeEmail: false,
+        changePassword: false,
+        changePublicKey: false,
+        currentPassword: "",
+        newPassword: "",
+        checkPassword: "",
+        publicKey: ""
+      },
+      infoRules: {
+        name: [],
+        stuid: [],
+        email: [
+          {
+            validator: validateEmail,
+            trigger: "blur"
+          }
+        ],
+        currentPassword: [
+          {
+            validator: validateCurrentPassword,
+            trigger: "blur"
+          }
+        ],
+        newPassword: [
+          {
+            validator: validateNewPassword,
+            trigger: "blur"
+          }
+        ],
+        checkPassword: [
+          {
+            validator: validateCheckPassword,
+            trigger: "blur"
+          }
+        ],
+        publicKey: []
+      },
+      oldEmail: "11410601@mail.sustc.edu.cn"
+    };
+  },
+  watch: {
+    "infoForm.email": function(newmail, oldmail) {
+      if (this.oldEmail === newmail) {
+        this.infoForm.changeEmail = false;
+      } else {
+        this.infoForm.changeEmail = true;
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$message("submit!");
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+};
+</script>
+
+<style scoped>
+.toVerifyInput {
+    width:69%
+}
+.toVerifyButton {
+    width:30%
+}
+
+.line {
+  text-align: center;
+}
+</style>
+
--- FrontEnd/src/views/ongoing/index.vue
+++ FrontEnd/src/views/ongoing/index.vue
@@ -1,40 +1,64 @@
 <template>
-    <el-row style="padding-right:20px" >
-        <el-col :span="5" v-for="(o) in 6" :offset="1" :key="o" class="card">
-            <el-card shadow="hover">
-                <img class="image" :src="hamburger" >
-                <div style="padding: 14px;">
-                    <el-row>
-                      <el-col :span="12"><span>离散数学</span></el-col>
-                      <el-col :span="9" :offset="3" ><span style="font-size: 13px;color: #666;">已参与：人</span></el-col>
-                    </el-row>
-                    
-                    
-                    <div class="bottom clearfix">
-                        <time class="time">结束时间：{{ currentDate }}</time>
-                        <el-button type="primary" style="margin-top:6px;width:100%">开始评教</el-button>
-                        <br>
-                        <el-button style="margin-top:6px;width:100%">查看结果</el-button>
-                    </div>
-                </div>
-            </el-card>
-        </el-col>
-        
+  <div>
+    <el-row style="padding-right:20px; padding-left:20px; width:100%" :gutter="20">
+      <el-col :span="6" v-for="(o) in 6" :offset="0" :key="o" class="card">
+        <el-card shadow="hover">
+          <img class="image" :src="hamburger">
+          <div style="padding: 14px;">
+            <el-row>
+              <el-col :span="24">
+                <span>Discrete Mathematics</span>
+              </el-col>
+              <el-col :span="24">
+                <span style="font-size: 13px;">Instructor: WANG Qi</span>
+              </el-col>
+            </el-row>
+
+            <div class="bottom clearfix">
+              <span style="font-size: 13px;color: #666;">Participation: 1/60</span>
+              <time class="time">End Time: {{ currentDate }}</time>
+              <el-button type="primary" style="margin-top:6px;width:100%">Start</el-button>
+              <br>
+              <el-button style="margin-top:6px;width:100%">Result</el-button>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
     </el-row>
-    
+    <el-row>
+      <el-col :span="24">
+        <div class="block" style="text-align: center; padding:60px">
+          <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="currentPage4" :page-sizes="[100, 200, 300, 400]" :page-size="100" layout="total, sizes, prev, pager, next, jumper" :total="400">
+          </el-pagination>
+        </div>
+
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
-import hamburger from '@/assets/hamburger.png'
+import hamburger from '@/assets/qwang.jpg'
 export default {
   name: 'ongoing',
   data() {
     return {
       hamburger,
-      currentDate: new Date().toLocaleTimeString()
+      currentDate: new Date().toLocaleTimeString(),
+      currentPage1: 5,
+      currentPage2: 5,
+      currentPage3: 5,
+      currentPage4: 4
+    }
+  },
+  methods: {
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`)
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`)
     }
   }
-
 }
 </script>
 
@@ -43,13 +67,12 @@ export default {
 <style>
 .card {
   margin-top: 40px;
-    
 }
 .time {
   font-size: 13px;
   color: #999;
   padding-bottom: 8px;
-  /* padding-top: 8px; */
+  padding-top: 8px;
   display: block;
 }
 
@@ -65,10 +88,8 @@ export default {
 
 .image {
   width: 100%;
-  max-width:400px;
+  max-width: 400px;
   display: block;
 }
-
-
 </style>
 
